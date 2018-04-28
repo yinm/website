f600936775ec958f59c718d6b65685ad14d659ee
yinm <yinm.cminor9@gmail.com>
2018-04-29 00:12:01 +0900

    Fix title

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,11 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Starter Blog',
-    author: 'Kyle Mathews',
-    description: 'A starter blog demonstrating what Gatsby can do.',
-    siteUrl: 'https://gatsbyjs.github.io/gatsby-starter-blog/',
+    title: '楽しいだけで十分です',
+    author: 'yinm',
+    description: 'yinm\'s blog',
+    siteUrl: 'https://yinm.netlify.com/',
   },
-  pathPrefix: '/gatsby-starter-blog',
+  pathPrefix: '/',
   plugins: [
     {
       resolve: `gatsby-source-filesystem`,
--- src/layouts/index.js
+++ src/layouts/index.js
@@ -30,7 +30,7 @@ class Template extends React.Component {
             }}
             to={'/'}
           >
-            Gatsby Starter Blog
+            楽しいだけで十分です
           </Link>
         </h1>
       )
@@ -51,7 +51,7 @@ class Template extends React.Component {
             }}
             to={'/'}
           >
-            Gatsby Starter Blog
+            楽しいだけで十分です
           </Link>
         </h3>
       )
