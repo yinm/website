49d9c232da4d738dd1797dedba9c26894474675a
yinm <yinm.cminor9@gmail.com>
2018-05-03 19:49:47 +0900

    Add config for fetching markdwon files

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,15 @@ module.exports = {
   siteMetadata: {
     title: '楽しいだけで十分です',
   },
-  plugins: ['gatsby-plugin-react-helmet'],
+  plugins: [
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        path: `${__dirname}/src/pages`,
+        name: 'pages',
+      },
+    },
+    'gatsby-plugin-react-helmet',
+    'gatsby-transformer-remark',
+  ],
 }
