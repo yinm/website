4e59b290468309cf11a1cb473e6d12424a3173f7
yinm <yinm.cminor9@gmail.com>
2018-05-03 20:07:02 +0900

    Add rendering posts

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,32 @@
 import React from "react"
 
-export default () =>
-  <div>
-    <h2>Index</h2>
-    <p>index page</p>
-  </div>
+export default ({ data }) => {
+  return (
+    <div>
+      {data.allMarkdownRemark.edges.map(({ node }) => (
+        <div key={node.id}>
+          <h3>{node.frontmatter.title}</h3>
+          <time>{node.frontmatter.date}</time>
+          <p>{node.excerpt}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export const pageQuery = graphql`
+  query IndexQuery {
+    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+      edges {
+        node {
+          id
+          excerpt
+          frontmatter {
+            title
+            date(formatString: "YYYY-MM-DD")
+          }
+        }
+      }
+    }
+  }
+`
