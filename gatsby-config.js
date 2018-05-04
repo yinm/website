65ebbee230197108a967f172983f81a6acb80df0
yinm <yinm.cminor9@gmail.com>
2018-05-04 21:14:47 +0900

    Add favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,6 +10,24 @@ module.exports = {
         name: 'pages',
       },
     },
+    {
+      resolve: `gatsby-plugin-favicon`,
+      options: {
+        logo: "./src/favicon.jpg",
+        injectHTML: true,
+        icons: {
+          android: true,
+          appleIcon: true,
+          appleStartup: true,
+          coast: false,
+          favicons: true,
+          firefox: true,
+          twitter: false,
+          yandex: false,
+          windows: false,
+        }
+      }
+    },
     'gatsby-plugin-react-helmet',
     'gatsby-transformer-remark',
   ],
