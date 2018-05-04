f5a89f56e6d1bc6ccd60bf521af80e1771abe33b
yinm <yinm.cminor9@gmail.com>
2018-05-04 22:00:48 +0900

    Remove favicon setting for deploy failure

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,24 +11,6 @@ module.exports = {
       },
     },
     {
-      resolve: `gatsby-plugin-favicon`,
-      options: {
-        logo: "./src/favicon.jpg",
-        injectHTML: true,
-        icons: {
-          android: true,
-          appleIcon: true,
-          appleStartup: true,
-          coast: false,
-          favicons: true,
-          firefox: true,
-          twitter: false,
-          yandex: false,
-          windows: false,
-        }
-      }
-    },
-    {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         trackingId: `UA-118700310-1`,
