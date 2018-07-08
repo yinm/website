c0915112d431d1d09d5bce15dbc2f35ef8b98b92
yinm <yinm.cminor9@gmail.com>
2018-07-08 15:12:00 +0900

    Add txt component

---
+++ src/components/atoms/Txt/index.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import styles from './styles.css'
+
+const txtFactory = role => ({
+  tag: Tag = 'p',
+  size = 'm',
+  className,
+  ...props
+}) => (
+  <Tag
+    className={ [
+      styles[role],
+      styles[size],
+      className
+    ].join(' ') }
+    { ...props }
+  />
+)
+
+const Txt = txtFactory('default')
+export default Txt
