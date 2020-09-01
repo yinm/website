20a61cf98dc212b44789d1e993d0fb0abc6c4614
yinm <yinm.cminor9@gmail.com>
2020-09-01 20:57:52 +0900

    Handle embed videos

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,15 +1,15 @@
 module.exports = {
   siteMetadata: {
-    title: '楽しいだけで十分です',
-    description: 'Frontend Engineerのyinmのブログです',
-    siteUrl: 'https://yinm.info',
+    title: "楽しいだけで十分です",
+    description: "Frontend Engineerのyinmのブログです",
+    siteUrl: "https://yinm.info",
   },
   plugins: [
     {
-      resolve: 'gatsby-source-filesystem',
+      resolve: "gatsby-source-filesystem",
       options: {
         path: `${__dirname}/src/pages`,
-        name: 'pages',
+        name: "pages",
       },
     },
     {
@@ -34,9 +34,34 @@ module.exports = {
       },
     },
     {
-      resolve: `gatsby-plugin-feed`
+      resolve: `gatsby-plugin-feed`,
     },
-    'gatsby-plugin-react-helmet',
+    {
+      resolve: "gatsby-transformer-remark",
+      options: {
+        plugins: [
+          {
+            resolve: "gatsby-remark-embed-video",
+            options: {
+              width: 800,
+              ratio: 1.77, // Optional: Defaults to 16/9 = 1.77
+              height: 400, // Optional: Overrides optional.ratio
+              related: false, //Optional: Will remove related videos from the end of an embedded YouTube video.
+              noIframeBorder: true, //Optional: Disable insertion of <style> border: 0
+              urlOverrides: [
+                {
+                  id: "youtube",
+                  embedURL: (videoId) =>
+                    `https://www.youtube-nocookie.com/embed/${videoId}`,
+                },
+              ], //Optional: Override URL of a service provider, e.g to enable youtube-nocookie support
+              containerClass: "embedVideo-container", //Optional: Custom CSS class for iframe container, for multiple classes separate them by space
+            },
+          },
+        ],
+      },
+    },
+    "gatsby-plugin-react-helmet",
     `gatsby-plugin-netlify`, // make sure to put last in the array
   ],
-}
+};
